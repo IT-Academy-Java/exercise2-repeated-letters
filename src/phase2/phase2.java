2bba61959e85aa466dec2b734bf05b872dad2f7d
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-26 18:12:18 +0200

    ADD phase2

---
+++ src/phase2/phase2.java
@@ -0,0 +1,33 @@
+package phase2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class phase2 {
+	public static void main(String[] args) {
+		char[] name = {'E','L','E','N','A','1'};
+		
+		List<Character> listName = new ArrayList<>();
+		
+		for (int i = 0; i < name.length; i++) {
+			listName.add(name[i]);
+		}
+		
+		Iterator<Character> it = listName.iterator();
+		while(it.hasNext()) {
+			Object aux = it.next();
+			if("AEIOUaeiou".contains(aux.toString())) {
+				System.out.println("Vocal");
+			} else if("0123456789".contains(aux.toString())) {
+				System.out.println("Els noms de persones no contenen números!");
+			} else {
+				System.out.println("Consonante");
+			}
+			
+			
+		}
+			  
+		
+	}
+}
