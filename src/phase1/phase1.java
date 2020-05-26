88319e30792152e0f5b3b7e60a67e07048a759c6
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-26 12:55:56 +0200

    ADD phase1

---
+++ src/phase1/phase1.java
@@ -0,0 +1,13 @@
+package phase1;
+
+public class phase1 {
+	public static void main(String[] args) {
+		
+		char[] name = {'E','L','E','N','A'};
+		
+		for(int i=0; i < name.length; i++) {
+			System.out.println(name[i]);
+		}
+	}
+
+}
---
+++ src/phase1/phase1.js
@@ -0,0 +1,9 @@
+/**
+ * phase 1
+ */
+
+const name = ['E','L','E','N','A'];
+
+for (let i = 0; i < name.length; i++) {
+	console.log(name[i]);
+}
