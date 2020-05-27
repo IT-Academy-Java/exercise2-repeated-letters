ebeb480bb22b12d316eb0433c743e06809af657d
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-27 12:22:15 +0200

    ADD phase 3 and phase 4 with javascript ... done

--- src/phase3/phase3.js
+++ src/phase3/phase3.js
@@ -1,3 +1,27 @@
 /**
- * 
+ * phase 3
  */
+
+const name = ["E", "L", "E", "N", "A"];
+const vocal = "aeiouAEIOU";
+const num = "0123456789";
+
+let mapLettersRepeated = new Map();
+
+for (let i = 0; i < name.length; i++) {
+  lletresRepetides(name[i], mapLettersRepeated);
+}
+
+function lletresRepetides(letter, map) {
+  if (map.has(letter)) {
+    let actualValue = map.get(letter) + 1;
+    map.set(letter, actualValue);
+  } else {
+    map.set(letter, 1);
+  }
+}
+
+for (var [clave, valor] of mapLettersRepeated.entries()) {
+  console.log(clave + " = " + valor);
+}
+
--- src/phase4/phase4.js
+++ src/phase4/phase4.js
@@ -1,3 +1,30 @@
 /**
- * 
+ * phase 4
  */
+
+const name = ["E", "L", "E", "N", "A"];
+const surname = ["P", "I", "A", "G", "G","I","O",];
+const fullName = [...name, " ", ...surname]
+const vocal = "aeiouAEIOU";
+const num = "0123456789";
+
+let mapLettersRepeated = new Map();
+
+for (let i = 0; i < name.length; i++) {
+  lletresRepetides(name[i], mapLettersRepeated);
+}
+
+function lletresRepetides(letter, map) {
+  if (map.has(letter)) {
+    let actualValue = map.get(letter) + 1;
+    map.set(letter, actualValue);
+  } else {
+    map.set(letter, 1);
+  }
+}
+
+for (var [clave, valor] of mapLettersRepeated.entries()) {
+  console.log(clave + " = " + valor);
+}
+
+console.log(fullName);
