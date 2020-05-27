4aa8650fe39ff1ef177870c9af102a66f7325435
Elena Piaggio <elenapiaggio@gmail.com>
2020-05-27 10:43:45 +0200

    UPDATE phase2

--- src/phase2/phase2.java
+++ src/phase2/phase2.java
@@ -6,28 +6,25 @@ import java.util.List;
 
 public class phase2 {
 	public static void main(String[] args) {
-		char[] name = {'E','L','E','N','A','1'};
-		
+		char[] name = { 'E', 'L', 'E', 'N', 'A', '1' };
+
 		List<Character> listName = new ArrayList<>();
-		
+
 		for (int i = 0; i < name.length; i++) {
 			listName.add(name[i]);
 		}
-		
+
 		Iterator<Character> it = listName.iterator();
-		while(it.hasNext()) {
+
+		while (it.hasNext()) {
 			Object aux = it.next();
-			if("AEIOUaeiou".contains(aux.toString())) {
+			if ("AEIOUaeiou".contains(aux.toString())) {
 				System.out.println("Vocal");
-			} else if("0123456789".contains(aux.toString())) {
+			} else if ("0123456789".contains(aux.toString())) {
 				System.out.println("Els noms de persones no contenen números!");
 			} else {
 				System.out.println("Consonante");
 			}
-			
-			
 		}
-			  
-		
 	}
 }
